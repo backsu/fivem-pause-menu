84588f37bc03bc02d551b1fd04ad69b51f44c89f
Sang Yoo Kim <sangyoo.km@gmail.com>
2021-02-05 19:37:38 +0900

    Fix typo

--- zombie_escmenu/nui/ui.js
+++ zombie_escmenu/nui/ui.js
@@ -16,54 +16,49 @@ function fnCopyToClipboard(str) {
 $(document).ready(function(){
 
     window.addEventListener( 'message', function( event ) {
-           var item = event.data;
-   
-                if ( item.open == true ) {
-                $('.container').css('display','block');
-            } else if ( item.open == false ) {
-                $('.container').css("display","none");
-            }
-       });
+        var item = event.data;
+        if ( item.open == true ) {
+            $('.container').css('display','block');
+        } else if ( item.open == false ) {
+            $('.container').css("display","none");
+        }
+    });
 
-  $("#Continue").click(function(){
+    $("#Continue").click(function(){
         $.post('http://zombie_escmenu/continue', JSON.stringify({}));2
     });
-  $("#discord").click(function(){
+    $("#discord").click(function(){
         $.post('http://zombie_escmenu/discord', JSON.stringify({}));2
 		/* fnCopyToClipboard("https://discord.gg/dUkuYGQ") */
     });
-  $("#guide").click(function(){
+    $("#guide").click(function(){
         $.post('http://zombie_escmenu/guide', JSON.stringify({}));2
     });
-  $("#key").click(function(){
+    $("#key").click(function(){
         $.post('http://zombie_escmenu/key', JSON.stringify({}));2
     });
-  $("#offgame").click(function(){
+    $("#offgame").click(function(){
         $.post('http://zombie_escmenu/offGame', JSON.stringify({}));2
     });
 
-   $("#close").click(function(){
+    $("#close").click(function(){
         $.post('http://zombie_escmenu/close', JSON.stringify({}));2
     });
-   
-   
-   })
+})
    
 $(document).ready(function(){
-
     window.addEventListener( 'message', function( event ) {
-           var item = event.data;
-   
-                if ( item.opendiscord == true ) {
-                $('.discordmenu').css('display','block');
-            } else if ( item.opendiscord == false ) {
-                $('.discordmenu').css("display","none");
-            }
-       });
-  $("#discorcopy").click(function(){
-        $.post('http://zombie_escmenu/discorcopy', JSON.stringify({}));2
+        var item = event.data;
+
+            if ( item.opendiscord == true ) {
+            $('.discordmenu').css('display','block');
+        } else if ( item.opendiscord == false ) {
+            $('.discordmenu').css("display","none");
+        }
+    });
+    $("#discorcopy").click(function(){
+        $.post('http://zombie_escmenu/discorcopy', JSON.stringify({}));
 		fnCopyToClipboard("https://discord.gg/dUkuYGQ")
     });
-   
-   
-   })
+ 
+})
