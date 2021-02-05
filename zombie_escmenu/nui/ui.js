dcf1cff6c99c76820fce7a72f437ac216db21de5
Sang Yoo Kim <sangyoo.km@gmail.com>
2021-02-05 19:38:08 +0900

    Remove lingering 2 typos

--- zombie_escmenu/nui/ui.js
+++ zombie_escmenu/nui/ui.js
@@ -25,32 +25,31 @@ $(document).ready(function(){
     });
 
     $("#Continue").click(function(){
-        $.post('http://zombie_escmenu/continue', JSON.stringify({}));2
+        $.post('http://zombie_escmenu/continue', JSON.stringify({}));
     });
     $("#discord").click(function(){
-        $.post('http://zombie_escmenu/discord', JSON.stringify({}));2
+        $.post('http://zombie_escmenu/discord', JSON.stringify({}));
 		/* fnCopyToClipboard("https://discord.gg/dUkuYGQ") */
     });
     $("#guide").click(function(){
-        $.post('http://zombie_escmenu/guide', JSON.stringify({}));2
+        $.post('http://zombie_escmenu/guide', JSON.stringify({}));
     });
     $("#key").click(function(){
-        $.post('http://zombie_escmenu/key', JSON.stringify({}));2
+        $.post('http://zombie_escmenu/key', JSON.stringify({}));
     });
     $("#offgame").click(function(){
-        $.post('http://zombie_escmenu/offGame', JSON.stringify({}));2
+        $.post('http://zombie_escmenu/offGame', JSON.stringify({}));
     });
 
     $("#close").click(function(){
-        $.post('http://zombie_escmenu/close', JSON.stringify({}));2
+        $.post('http://zombie_escmenu/close', JSON.stringify({}));
     });
 })
    
 $(document).ready(function(){
     window.addEventListener( 'message', function( event ) {
         var item = event.data;
-
-            if ( item.opendiscord == true ) {
+        if ( item.opendiscord == true ) {
             $('.discordmenu').css('display','block');
         } else if ( item.opendiscord == false ) {
             $('.discordmenu').css("display","none");
